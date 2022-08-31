9fa63e57e930db5942e67ec58af296b7b9402763
nan0scho1ar <scorch267@gmail.com>
2022-08-31 11:21:48 +1000

    Added doom emacs config

---
+++ doom/.doom.d/packages.el
@@ -0,0 +1,37 @@
+(package! smooth-scroll)
+
+(package! beacon)
+
+(package! vlf :recipe (:host github :repo "m00natic/vlfi" :files ("*.el"))
+  :pin "cc02f2533782d6b9b628cec7e2dcf25b2d05a27c" :disable t)
+
+(package! rebox2)
+
+(package! yasnippet-snippets)
+
+(package! org-alert)
+
+(package! org-super-agenda)
+
+(package! org-roam-ui)
+
+(package! org-modern)
+
+(package! org-jira)
+
+(package! org-auto-tangle :recipe (:host github :repo "yilkalargaw/org-auto-tangle" :files ("*.el")))
+
+(package! ox-confluence :recipe (:host github :repo "nan0scho1ar/ox-confluence-modern" :files ("*.el")))
+
+(package! sqleye :recipe (:host github :repo "nan0scho1ar/sqleye" :files ("*.el")))
+
+(package! gitmanager :recipe (:host github :repo "nan0scho1ar/gitmanager" :files ("*.el")))
+
+(package! ansi-exec :recipe (:host github :repo "nan0scho1ar/emacs-ansi-exec" :files ("*.el")))
+(package! ahoy :recipe (:host github :repo "nan0scho1ar/emacs-ahoy" :files ("*.el")))
+
+(package! websocket)
+
+(package! simple-httpd)
+
+(package! f)
