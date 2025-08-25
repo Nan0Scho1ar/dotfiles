91d99f44cf2895e1a637125d9606f0e58ca1de91
nan0scho1ar <scorch267@gmail.com>
2025-08-25 13:49:30 +1000

    Add nushell to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-stow -t $HOME --no-folding -v -R alacritty autostart bash bspwm doom picom polybar sxhkd tmux vim xorg
+stow -t $HOME --no-folding -v -R alacritty autostart bash bspwm doom picom polybar sxhkd tmux vim xorg nushell
